ff21d362aaf6a2a377da863d074c357c20545dab
Nathan Blos <nb@blos-gesunde-rezepte.de>
2025-12-30 15:20:34 +0100

    Initial Commit

---
+++ BlosKochbuch/BlosKochbuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlosKochbuchApp.swift
+//  BlosKochbuch
+//
+//  Created by Nathan Blos on 30.12.25.
+//
+
+import SwiftUI
+
+@main
+struct BlosKochbuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
